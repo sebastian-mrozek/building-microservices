693423edf4f2bf9bf4454e533b6e5c05d9884c50
sebastian-mrozek <sebastian.mrozek@gmail.com>
2020-11-29 11:05:39 +1300

    Add a basic Javalin/Avaje based rest server

---
+++ task-list-backend/src/main/java/seb/xmpl/micro/Application.java
@@ -0,0 +1,31 @@
+package seb.xmpl.micro;
+
+import io.avaje.http.api.WebRoutes;
+import io.avaje.inject.SystemContext;
+import io.javalin.Javalin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Random;
+
+public class Application {
+
+    static Logger LOG = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        Javalin server = Javalin.create();
+
+        server.get("test", context -> {
+           context.result("Test " + new Random().nextLong());
+        });
+
+//        server.routes(() -> new TaskController$route(new TaskController()).registerRoutes());
+
+        List<WebRoutes> webRoutes = SystemContext.context().getBeans(WebRoutes.class);
+        LOG.info("Found routes: {}", webRoutes);
+
+        server.routes(() -> webRoutes.forEach(WebRoutes::registerRoutes));
+        server.start();
+    }
+}
